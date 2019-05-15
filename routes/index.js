1b326b7dee460bc1472aff46dcfbb8381934bf31
arthurlapertosa <arthurlisboa@cpejr.com.br>
2019-05-15 15:04:52 -0300

    Inicio modificacoes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// Home
+router.get('/home', function(req, res, next) {
+  res.render('home', { title: 'Home' });
+});
+
 module.exports = router;
